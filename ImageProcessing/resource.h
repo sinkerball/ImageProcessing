17556a6fa1f11b37b8a1fb39999d3ab81591620f
sinkerball <missmm936@gmail.com>
2023-03-31 14:21:36 +0900

    초기 커밋.

---
+++ ImageProcessing/resource.h
@@ -0,0 +1,114 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// ImageProcessing.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_POPUP_EDIT                  119
+#define ID_STATUSBAR_PANE1              120
+#define ID_STATUSBAR_PANE2              121
+#define IDS_STATUS_PANE1                122
+#define IDS_STATUS_PANE2                123
+#define IDS_TOOLBAR_STANDARD            124
+#define IDS_TOOLBAR_CUSTOMIZE           125
+#define ID_VIEW_CUSTOMIZE               126
+#define IDR_MAINFRAME                   128
+#define IDR_MAINFRAME_256               129
+#define IDR_ImageProcessingTYPE         130
+#define ID_WINDOW_MANAGER               131
+#define IDR_THEME_MENU                  200
+#define ID_SET_STYLE                    201
+#define ID_VIEW_APPLOOK_WIN_2000        205
+#define ID_VIEW_APPLOOK_OFF_XP          206
+#define ID_VIEW_APPLOOK_WIN_XP          207
+#define ID_VIEW_APPLOOK_OFF_2003        208
+#define ID_VIEW_APPLOOK_VS_2005         209
+#define ID_VIEW_APPLOOK_VS_2008         210
+#define ID_VIEW_APPLOOK_OFF_2007_BLUE   215
+#define ID_VIEW_APPLOOK_OFF_2007_BLACK  216
+#define ID_VIEW_APPLOOK_OFF_2007_SILVER 217
+#define ID_VIEW_APPLOOK_OFF_2007_AQUA   218
+#define ID_VIEW_APPLOOK_WINDOWS_7       219
+#define IDS_EDIT_MENU                   306
+#define IDD_DIALOG11                    312
+#define IDD_DIALOG12                    315
+#define IDD_DIALOG13                    318
+#define IDD_DIALOG14                    321
+#define IDD_DIALOG15                    324
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1002
+#define ID_32771                        32771
+#define ID_DOWN_SAMPLE                  32772
+#define ID_32773                        32773
+#define ID_UP_SAMPLING                  32774
+#define ID_32775                        32775
+#define ID_QUANTIZATION                 32776
+#define ID_32777                        32777
+#define ID_SUM_CONSTANT                 32778
+#define ID_32779                        32779
+#define ID_SUB_CONSTANT                 32780
+#define ID_32781                        32781
+#define ID_MUL_CONSTANT                 32782
+#define ID_32783                        32783
+#define ID_DIV_CONSTANT                 32784
+#define ID_32785                        32785
+#define ID_AND_OPERATE                  32786
+#define ID_32787                        32787
+#define ID_OR_OPERATE                   32788
+#define ID_32789                        32789
+#define ID_XOR_OPERATE                  32790
+#define ID_32791                        32791
+#define ID_NEGA_TRANSFORM               32792
+#define ID_32793                        32793
+#define ID_GAMMA_CORRECTION             32794
+#define ID_32795                        32795
+#define ID_BINARIZATION                 32796
+#define ID_32797                        32797
+#define ID_STRESS_TRANSFORM             32798
+#define ID_32799                        32799
+#define ID_HISTO_STRETCH                32800
+#define ID_32801                        32801
+#define ID_END_IN_SEARCH                32802
+#define ID_32803                        32803
+#define ID_Menu                         32804
+#define ID_HISTOGRAM                    32805
+#define ID_32806                        32806
+#define ID_HISTO_EQUAL                  32807
+#define ID_32808                        32808
+#define ID_HISTO_SPEC                   32809
+#define ID_EMBOSSING                    32810
+#define ID_32811                        32811
+#define ID_BLURR                        32812
+#define ID_32813                        32813
+#define ID_GUASSIAN_FILTER              32814
+#define ID_32815                        32815
+#define ID_SHARPENING                   32816
+#define ID_32817                        32817
+#define ID_HPF_SHARP                    32818
+#define ID_32819                        32819
+#define ID_LPF_SHARP                    32820
+#define ID_32821                        32821
+#define ID_DIFF_OPERATOR_HOR            32822
+#define ID_32823                        32823
+#define ID_HOMOGEN_OPERATOR             32824
+#define ID_32825                        32825
+#define ID_LAPLACIAN                    32826
+#define ID_32827                        32827
+#define ID_NEAREST                      32828
+#define ID_32829                        32829
+#define ID_BILINEAR                     32830
+#define ID_32831                        32831
+#define ID_MEDIAN_SUB                   32832
+#define ID_32833                        32833
+#define ID_MEAN_SUB                     32834
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        325
+#define _APS_NEXT_COMMAND_VALUE         32835
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           325
+#endif
+#endif
